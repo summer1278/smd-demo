487d2193480634e52201a2a0f6d23b51bcf3378d
Xia Cui <summer1278@users.noreply.github.com>
2014-07-08 14:32:22 +0100

    unfixed problem: undefined ref

--- scripts/load_and_parse_from_segments.js
+++ scripts/load_and_parse_from_segments.js
@@ -1,1 +1,16 @@
-data = <%= @new_json.html_safe %>
+var jQuery = require('jQuery');
+print("hello world!");
+var data = jQuery.parseJSON("./audio/test.json");
+
+/*
+$.getJSON('./audio/test.json', function(data) {
+        var output="<ul>";
+        for (var i in data.users) {
+            output+="<li>" + data.users[i].firstName + " " + data.users[i].lastName + "--" + data.users[i].joined.month+"</li>";
+        }
+
+        output+="</ul>";
+        document.getElementById("placeholder").innerHTML=output;
+  });
+*/
+
