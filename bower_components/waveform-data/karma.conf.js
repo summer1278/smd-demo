389f81b6948c3ccbd7a64005d8f9a6f3f61f9115
Xia Cui <cxcui188@gmail.com>
2014-07-09 17:45:42 +0100

    bower package

---
+++ bower_components/waveform-data/karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function(config){
+  var isCI = Boolean(process.env.CI);
+
+  config.set({
+    basePath: '',
+    files: [
+      { pattern: 'lib/**/*.js', included: false, served: false, watched: true },
+      'test/unit/*.js'
+    ],
+    frameworks: ['mocha', 'chai', 'browserify'],
+    preprocessors: {
+      'test/**/*.js': ['browserify'],
+      'waveform-data.js': ['browserify']
+    },
+    browserify: {
+      external: ['dist/waveform-data.min.js']
+    },
+    plugins: [
+      'karma-*'
+    ],
+    exclude: [],
+    reporters: isCI ? 'dots' : 'progress',
+    port: 8080,
+    runnerPort: 9100,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: !isCI,
+    browsers: isCI ? ['PhantomJS'] : ['Chrome', 'Safari', 'Firefox' ],
+    captureTimeout: 5000,
+    singleRun: isCI
+  });
+};
