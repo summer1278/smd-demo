0b0bba56a43303c527505173abbe14405b9e369e
Xia Cui <cxcui188@gmail.com>
2014-07-21 13:47:08 +0100

    fix naming

--- scripts/add_times_to_cfa_output.rb
+++ scripts/add_times_to_cfa_output.rb
@@ -1,23 +1,24 @@
 require 'json'
 require 'pp'
 
-def num_mfcc_frames_in_cfa_frames( num_cfa_frames, block_size, step_size )
-  block_size + ( num_cfa_frames - 1 ) * step_size
+def num_frames_in_a_block( num_blocks, block_size, step_size )
+  block_size + ( num_blocks - 1 ) * step_size
 end
 
-def num_samples_in_mfcc_frames( num_mfcc_frames, block_size, step_size )
-  block_size + ( num_mfcc_frames - 1 ) * step_size
+def num_samples_in_a_window( num_frames, block_size, step_size )
+  block_size + ( num_frames - 1 ) * step_size
 end
 
-def time_of_one_window (window_block_size, sample_rate, frame_block_size)
-  window_block_size / sample_rate * frame_block_size
+def time_of_one_window (window_block_size, sample_rate, block_frame_size)
+  window_block_size / sample_rate * block_frame_size
 end
 
 file     = File.open('audio/test.mp3.cfa_2.2.csv') # 2.2 seems to be best fit for this sample
 
 lines    = file.to_a
 total_segments = lines.size
-puts total_segments
+#puts total_segments
+
 # some presits
 time_slot = time_of_one_window(1024.0, 11025.0, 100.0)
 
@@ -33,12 +34,12 @@ lines.each do |line|
   number   = line.to_i
   line_num = line_num + 1
 
-  num_mfcc    = num_mfcc_frames_in_cfa_frames( line_num, 100.0, 50.0 )
-  num_samples = num_samples_in_mfcc_frames( num_mfcc, 1024.0, 512.0 )
+  num_frames   = num_frames_in_a_block( line_num, 100.0, 50.0 )
+  num_samples = num_samples_in_a_window( num_frames, 1024.0, 512.0 ) 
   time_in_sec = num_samples / 11025.0
   
   
-  #say music is red, no_music is green.
+  # say music is red, no_music is green.
   if number == 1 
 	color = 'rgba(215, 40, 40, 0.9)' # red
   else
@@ -49,17 +50,18 @@ lines.each do |line|
   #hash
   data = { }
   
-  temp_start = time_in_sec - time_slot + time_start_offset #add offset
+  temp_start = time_in_sec - time_slot + time_start_offset # add offset
   if temp_start < 0
 	temp_start =  0 # remove negative start times
   end
   
   temp_end = time_in_sec - time_end_offset # substract offset
   
-  if line_num == total_segments #total_segments
+  if line_num == total_segments 
 	temp_end = time_in_sec # last: to the end
   end
   
+  # merge same-label segments into one
   if list.last && list.last[:endTime] == temp_start.round(2) && list.last[:color] == color
     list.last[:endTime]  =  temp_end.round(2)
   else
