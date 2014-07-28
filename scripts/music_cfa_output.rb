45ec6b448b659f9352dda7f4a65ca86555335b26
Xia Cui <cxcui188@gmail.com>
2014-07-28 14:24:28 +0100

    add cfa result

--- scripts/music_cfa_output.rb
+++ scripts/music_cfa_output.rb
@@ -1,19 +1,46 @@
 require 'taglib'
+require 'csv'
+#ruby ./scripts/music_cfa_output.rb
+feature_plan = 'yaafe.py -r 11025 --resample -f "cfa: SimpleNoiseGate>ContinuousFrequencyActivation BinThreshold=10 NbPeaks=40>WindowConvolution WCLength=17>SimpleThresholdClassification STCThreshold=2.2" -p Metadata=False '
 
-Dir.glob('music/*.mp3') do |mp3_file|
-  TagLib::FileRef.open(mp3_file) do |fileref|
-  unless fileref.null?
-    tag = fileref.tag 
-    tag.title   
-    tag.artist  
-    tag.album   
-    tag.genre   
+CSV.open('music/mp3_output.csv', 'w') do |music_csv|
+# header
+music_csv << ['Title','Type', 'Classification','Genre','Duration(in secs)']
+# for all audio files
+	Dir.glob('music/*.mp3') do |mp3_file|
+	#gather metadata
+	title = ''
+	clasf = ''
+	genre = ''
+	duration = ''
+		TagLib::FileRef.open(mp3_file) do |fileref|
+			unless fileref.null?
+			tag = fileref.tag 
+			title = tag.title   
+			#tag.artist  
+			#tag.album   
+			genre = tag.genre   
 
-    properties = fileref.audio_properties
-    properties.length  
-    p tag.title
-    p tag.genre
-    p properties.length
-  end
-end 
+			properties = fileref.audio_properties
+			duration = properties.length  
+			end
+		end 
+   
+    # generate cfa output
+	system(feature_plan+mp3_file)
+	# sum-up classification result
+	file = File.open(mp3_file+'.cfa.csv')
+	lines = file.to_a.map(&:to_i)
+	if lines.include?(0)
+		clasf = 'S'
+		if lines.include?(1)
+			clasf = clasf + '&M'
+		end
+	end
+	if lines.include?(1) && !lines.include?(0) 
+		clasf = 'M'
+	end
+	# add this entry
+	music_csv << [title,'M', clasf, genre, duration]
+	end
 end
