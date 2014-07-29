946edd23c0818728db3e3bfe37ce4d30c7efa7d5
Matt Haynes <matt@matthaynes.net>
2014-07-29 14:53:39 +0100

    Ensure extra packages for m4a->mp3 conversion installed. Add conversion step if input file is not mp3

--- lib/smd/import_audio.rb
+++ lib/smd/import_audio.rb
@@ -5,58 +5,66 @@ require 'fileutils'
 
 module Smd
 
-class ImportAudio
-  def initialize( input_file, type, output_directory )
-    @input_file = input_file
-    @type = type
-    @output_directory = output_directory
-    @uuid = SecureRandom.uuid
-  end
+  class ImportAudio
+    def initialize( input_file, type, output_directory )
+      @input_file = input_file
+      @type = type
+      @output_directory = output_directory
+      @uuid = SecureRandom.uuid
+    end
 
-  def import
-  	copy_mp3
-	generate_cfa_data
-	generate_waveform
-	generate_metadata
-  end
+    def import
+      copy_mp3
+      generate_cfa_data
+      generate_waveform
+      generate_metadata
+    end
 
-  def generate_cfa_data
-    feature_plan =  'yaafe.py -r 11025 --resample'
-    feature_plan += ' -f "cfa: SimpleNoiseGate>ContinuousFrequencyActivation '
-    feature_plan += 'BinThreshold=10 NbPeaks=40>WindowConvolution WCLength=17" '
-    feature_plan += '-p Metadata=False'
+    def generate_cfa_data
+      feature_plan =  'yaafe.py -r 11025 --resample'
+      feature_plan += ' -f "cfa: SimpleNoiseGate>ContinuousFrequencyActivation '
+      feature_plan += 'BinThreshold=10 NbPeaks=40>WindowConvolution WCLength=17" '
+      feature_plan += '-p Metadata=False'
 
-    system(feature_plan + ' '+ @output_directory+'/'+@uuid+'.mp3')
-  end
+      system( feature_plan + ' ' + results_file('mp3') )
+    end
 
-  def generate_waveform
-    audio_cmd = 'audiowaveform -i '#test.mp3 -o test.dat
-    audio_setting = ' -z 256 -b 8'
-    system(audio_cmd+@input_file+' -o ' +@output_directory+'/'+@uuid+'.dat '+audio_setting)
-  end
+    def generate_waveform
+      audio_cmd = 'audiowaveform -i '#test.mp3 -o test.dat
+      audio_setting = ' -z 256 -b 8'
+      system( results_file('mp3') +' -o ' + results_file('dat') + audio_setting)
+    end
 
-  def generate_metadata
-    TagLib::FileRef.open(@input_file) do |fileref|
-      unless fileref.null?
-        tag = fileref.tag
-        #title = tag.title
-        #tag.artist
-        #tag.album
-        #genre = tag.genre
+    def generate_metadata
+      TagLib::FileRef.open( results_file('mp3') ) do |fileref|
+        unless fileref.null?
+          tag = fileref.tag
+          #title = tag.title
+          #tag.artist
+          #tag.album
+          #genre = tag.genre
 
-        properties = fileref.audio_properties
-        #duration = properties.length
-        CSV.open(@output_directory+'/'+@uuid+'.metadata.csv', 'w') do |music_csv|
-	      music_csv << [tag.title, tag.artist, 'M', tag.genre, properties.length]
+          properties = fileref.audio_properties
+          #duration = properties.length
+          CSV.open( results_file('metadata.csv'), 'w') do |music_csv|
+            music_csv << [tag.title, tag.artist, 'M', tag.genre, properties.length]
+          end
         end
       end
     end
-  end
 
-  def copy_mp3
-    FileUtils.cp( @input_file, @output_directory+'/'+@uuid+'.mp3' )
+    def copy_mp3
+      if @input_file =~ /\.mp3$/
+        FileUtils.cp( @input_file, results_file('mp3') )
+      else
+        system("avconv -i \"#{@input_file}\" #{results_file('mp3')}")
+      end
+    end
+
+    def results_file(type)
+      File.join( @output_directory, @uuid + '.' + type )
+    end
   end
-end
 
 end
 
--- provisioning/install-ffmpeg.sh
+++ provisioning/install-ffmpeg.sh
@@ -4,5 +4,4 @@ sudo add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ precise univer
 sudo add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ precise-updates universe multiverse"
 sudo apt-get update
 sudo apt-get install -y ffmpeg
-sudo apt-get install -y ubuntu-restricted-extras
-
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' -f -q -y install ubuntu-restricted-extras
