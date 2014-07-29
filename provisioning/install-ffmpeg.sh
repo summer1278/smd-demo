9856411b0304f8aac89496e70be4bc65b90230a4
Matt Haynes <matt@matthaynes.net>
2014-07-29 14:38:10 +0100

    Add restricted extras

--- provisioning/install-ffmpeg.sh
+++ provisioning/install-ffmpeg.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sudo add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ precise universe multiverse"
+sudo add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ precise-updates universe multiverse"
+sudo apt-get update
 sudo apt-get install -y ffmpeg
+sudo apt-get install -y ubuntu-restricted-extras
 
