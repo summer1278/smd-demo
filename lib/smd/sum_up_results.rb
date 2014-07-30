c6816dd6f922296fc7a62c29e869dcb6c5f2e899
Xia Cui <cxcui188@gmail.com>
2014-07-30 11:00:55 +0100

    search for csvs and process with only csvs

--- lib/smd/sum_up_results.rb
+++ lib/smd/sum_up_results.rb
@@ -10,8 +10,8 @@ class SumUpResults
   def sumUp
     CSV.open(@result_directory+'/mp3_output.csv', 'w') do |music_csv|
       music_csv << ['Title', 'Artist', 'Type', 'Genre', 'Duration(in secs)', 'Average CFA', 'CFA correct percentage']
-      Dir.glob(@result_directory+'/**/*.cfa.csv') do |mp3_file|
-  	    file = File.open(mp3_file)
+      Dir.glob(@result_directory+'/**/*.cfa.csv') do |cfa_csv_file|
+  	    file = File.open(cfa_csv_file)
         lines = file.to_a#.map(&:to_i)
         avg_CFA = lines.reduce(0.0){ |sum, el| sum + el.to_f }.to_f / lines.size
 
@@ -26,7 +26,7 @@ class SumUpResults
         ones = classified.select { |i| i == 1 }.size
         percentage = ones.to_f/classified.size.to_f
 	    
-	    header = CSV.read(mp3_file.gsub('.mp3.cfa.csv', '.metadata.csv')).first
+	    header = CSV.read(cfa_csv_file.gsub('.mp3.cfa.csv', '.metadata.csv')).first
 	    header << avg_CFA
 	    header << percentage
 	    music_csv << header
