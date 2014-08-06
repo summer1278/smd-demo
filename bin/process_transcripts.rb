79840a2867b3f51a17a12c481acd5ab747eb2274
Xia Cui <cxcui188@gmail.com>
2014-08-06 15:41:28 +0100

    add name problem

--- bin/process_transcripts.rb
+++ bin/process_transcripts.rb
@@ -22,15 +22,27 @@ end
 
 def download_mp3s
   FileUtils::mkdir_p '/data/speech/desert-island-discs'
-  system 'cd new'
   Dir.glob('/data/speech/desert-island-discs-transcripts/*.csv') do |csv_file|
     file = CSV.read(csv_file)
     #file[6][1]
+    file_name = file[6][1] + '.mp3'
+    if file[6][1].contains?('.mp3')
+      file_name = file[6][1]
+    end
     cmd = 'wget http://downloads.bbc.co.uk/podcasts/radio4/dida/'
-    cmd += file[6][1]+ '.mp3 -P ' +'/data/speech/desert-island-discs/'
+    cmd += file_name+' -P ' +'/data/speech/desert-island-discs/'
     system cmd
   end
 end
 
+def compare_filenames
+  Dir.glob('/data/speech/desert-island-discs-transcripts/*.csv') do |csv_file|
+    file = CSV.read(csv_file)
+    if File.exists?('/data/speech/desert-island-discs/'+file[6][1] + '.mp3') == false
+      p file[6][1]
+    end
+  end
+end
 #convert_csvs
-download_mp3s
+#download_mp3s
+compare_filenames
