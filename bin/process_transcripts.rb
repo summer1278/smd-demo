44417343325d9aa4307a7a17e3faad11f9c7c14b
Xia Cui <cxcui188@gmail.com>
2014-08-11 13:24:03 +0100

    fix read

--- bin/process_transcripts.rb
+++ bin/process_transcripts.rb
@@ -108,7 +108,7 @@ end
 
 def copy_truth
   Dir.glob('/data/results/speech/desert-island-discs/*.txt') do |txt_file|
-    path = File.read(txt_file).flattern
+    path = File.read(txt_file)
     p path
     file_name = File.basename(path).gsub('.mp3','.truth.csv')
     system ('cp -r /data/incoming/'+file_name +' /data/results/speech/desert-island-discs/')
