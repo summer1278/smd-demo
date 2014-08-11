b2bc277448495d602c0d30d9e69b64a9b99ef39d
Xia Cui <cxcui188@gmail.com>
2014-08-11 12:31:04 +0100

    fix time to seconds

--- bin/process_transcripts.rb
+++ bin/process_transcripts.rb
@@ -56,47 +56,55 @@ def compare_filenames
   p count
 end
 
-  def generate_truth 
-     Dir.glob('/data/speech/desert-island-discs-transcripts/*.csv') do |transcript|
-      file_name = File.basename(transcript).gsub('.csv', '.truth.csv')
-      file = CSV.read(transcript)
-      segments = []
-      segment = Array.new(3, nil)
-      file.drop(14).each do |line|
-        if segments.last && segments.last[1] == time_to_seconds(line[1]) && segments.last[2] == 'Music' &&
-          line[3] == nil
-          segments.last[1] = time_to_seconds(line[2])
-        elsif segments.last && segments.last[1] == time_to_seconds(line[1]) && segments.last[2] == 'Speech' &&
-          line[3] != nil
-          segments.last[1] = time_to_seconds(line[2])
-        else
-          segment = Array.new(3, nil)
-          if line[0] == 'Music' || line[0] == 'Opening Credits' || 
-            line[0] == 'Closing Credits' || line[3] != nil
-            segment[0] = time_to_seconds(line[1])
-            segment[1] = time_to_seconds(line[2])
-            
-            if line[0] == 'Music' || line[0] == 'Opening Credits' || 
-              line[0] == 'Closing Credits'
-              segment[2] = 'Music'
-            elsif line[3] != nil
-              segment[2] = 'Speech'
-            end
-          elsif line[3] == nil
-            segment[0] = time_to_seconds(line[1])
-            segment[1] = time_to_seconds(line[2])
-            segment[2] = 'Music'
-          end
-          segments << segment
-        end
+def time_to_seconds ( time )
+  if dt = DateTime.parse(time) rescue false 
+   dt.hour * 3600 + dt.min * 60 +dt.sec
+ end
+end
 
+def generate_truth 
+ Dir.glob('/data/speech/desert-island-discs-transcripts/*.csv') do |transcript|
+  file_name = File.basename(transcript).gsub('.csv', '.truth.csv')
+  file = CSV.read(transcript)
+  segments = []
+  segment = Array.new(3, nil)
+  file.drop(14).each do |line|
+    if segments.last && segments.last[1] == time_to_seconds(line[1]) && segments.last[2] == 'Music' &&
+      line[3] == nil
+      segments.last[1] = time_to_seconds(line[2])
+    elsif segments.last && segments.last[1] == time_to_seconds(line[1]) && segments.last[2] == 'Speech' &&
+      line[3] != nil
+      segments.last[1] = time_to_seconds(line[2])
+    else
+      segment = Array.new(3, nil)
+      if line[0] == 'Music' || line[0] == 'Opening Credits' || 
+        line[0] == 'Closing Credits' || line[3] != nil
+        segment[0] = time_to_seconds(line[1])
+        segment[1] = time_to_seconds(line[2])
+        
+        if line[0] == 'Music' || line[0] == 'Opening Credits' || 
+          line[0] == 'Closing Credits'
+          segment[2] = 'Music'
+        elsif line[3] != nil
+          segment[2] = 'Speech'
+        end
+      elsif line[3] == nil
+        segment[0] = time_to_seconds(line[1])
+        segment[1] = time_to_seconds(line[2])
+        segment[2] = 'Music'
       end
-      pp segments
-      CSV.open('/data/speech/desert-island-discs/'+file_name, 'w') do |csv_file|
-        segments.each {|row| csv_file<<row}
-      end
+      segments << segment
     end
+
   end
+  pp segments
+  CSV.open('/data/speech/desert-island-discs/'+file_name, 'w') do |csv_file|
+    segments.each {|row| csv_file<<row}
+  end
+end
+end
+
+
 #convert_csvs
 #download_mp3s
 compare_filenames
