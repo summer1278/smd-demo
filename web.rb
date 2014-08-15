78be53f226dce78c3f3a4030da920eca5a256350
Xia Cui <cxcui188@gmail.com>
2014-08-15 15:12:13 +0100

    add basic test page using sinatra

---
+++ web.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'sinatra'
+
+# Index Page
+get '/' do
+  @page_title = 'MP3 list'
+  erb(:index) 
+end
+
+# Results page
+get '/item/:uuid' do
+  @uuid = params['uuid']
+  erb(:result)
+end
+
+# Serve data
+get '/data/*' do
+  @data_directory = 'data'
+  send_file @data_directory + '/' + params[:splat].join('/')
+end
+
