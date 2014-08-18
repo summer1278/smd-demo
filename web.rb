40e0808b299c46ed1e3f75786b811ed7e485c29b
Xia Cui <cxcui188@gmail.com>
2014-08-18 13:30:21 +0100

    add single version

--- web.rb
+++ web.rb
@@ -4,14 +4,23 @@ require 'sinatra'
 # Index Page
 get '/' do
   @page_title = 'Radio List'
+  @input_directory = './data/**/'
   erb(:index) 
 end
 
-# Results page
+# Combined Results page
 get '/item/:uuid' do
   @uuid = params['uuid']
   @input_directory = './data/**/'
   erb(:result)
+end
+
+# Single Results page
+get '/item/:uuid/:type' do
+  @uuid = params['uuid']
+  @type = params['type']
+  @input_directory = './data/**/'
+  erb(:single_result)
 end
 
 # Serve data
